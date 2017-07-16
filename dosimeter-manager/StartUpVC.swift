d71cb95248416cd4b50ae4a58b1d1e3ae1b63681
xiaosj <xiaosj@gmail.com>
2017-07-15 18:05:07 -0700

    This branch can download the CVS file and show it in an interactive view.

--- dosimeter-manager/StartUpVC.swift
+++ dosimeter-manager/StartUpVC.swift
@@ -8,7 +8,10 @@
 
 import UIKit
 
-class StartUpVC: UIViewController {
+class StartUpVC: UIViewController, URLSessionDownloadDelegate, UIDocumentInteractionControllerDelegate {
+
+    var downloadTask: URLSessionDownloadTask!
+    var backgroundSession: URLSession!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +23,59 @@ class StartUpVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func downloadFile(_ sender: Any) {
+        let backgroundSessionConfiguration = URLSessionConfiguration.background(withIdentifier: "backgroundSession")
+        backgroundSession = URLSession(configuration: backgroundSessionConfiguration, delegate: self, delegateQueue: OperationQueue.main)
 
+        let downloadURL = URL(string: "http://slac.stanford.edu/~xiaosj/scanner/scan_test.csv")!
+        downloadTask = backgroundSession.downloadTask(with: downloadURL)
+        downloadTask.resume()
+    }
+
+    func urlSession(_ session: URLSession,
+                    downloadTask: URLSessionDownloadTask,
+                    didFinishDownloadingTo location: URL) {
+        let path = NSSearchPathForDirectoriesInDomains(FileManager.SearchPathDirectory.documentDirectory, FileManager.SearchPathDomainMask.userDomainMask, true)
+        let documentDirectoryPath:String = path[0]
+        let fileManager = FileManager()
+        let destinationURLForFile = URL(fileURLWithPath: documentDirectoryPath.appending("/history.csv"))
+        
+        if fileManager.fileExists(atPath: destinationURLForFile.path) {
+            showFileWithPath(path: destinationURLForFile.path)
+        }
+        else {
+            do {
+                try fileManager.moveItem(at: location, to: destinationURLForFile)
+                showFileWithPath(path: destinationURLForFile.path)
+            } catch {
+                print("An error occurred while moving file to destination url")
+            }
+        }
+    }
+
+    func urlSession(_ session: URLSession,
+                    task: URLSessionTask,
+                    didCompleteWithError error: Error?){
+        downloadTask = nil
+        if (error != nil) {
+            print(error!.localizedDescription)
+        }else{
+            print("The task finished transferring data successfully")
+        }
+    }
+    
+    func showFileWithPath(path: String){
+        let isFileFound:Bool? = FileManager.default.fileExists(atPath: path)
+        if isFileFound == true{
+            let viewer = UIDocumentInteractionController(url: URL(fileURLWithPath: path))
+            viewer.delegate = self
+            viewer.presentPreview(animated: true)
+        }
+    }
+
+    func documentInteractionControllerViewControllerForPreview(_ controller: UIDocumentInteractionController) -> UIViewController
+    {
+        return self
+    }
 }
 
